8a85c088f9d89ac269d3fad279159ffd1e6bc571
SriAashritha03 <sriaashrikarthi@gmail.com>
2026-01-17 23:37:13 +0530

    Home.py updated

--- routes/home.py
+++ routes/home.py
@@ -1,20 +1,27 @@
 from flask import Blueprint, render_template, request, session
 import os
-
-os.makedirs("static/uploads", exist_ok=True)
-os.makedirs("static/outputs", exist_ok=True)
+import time
+from werkzeug.utils import secure_filename
 
 home_bp = Blueprint('home', __name__)
 UPLOAD_FOLDER = 'static/uploads'
 
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @home_bp.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         file = request.files.get('image')
         if file:
-            save_path = os.path.join(UPLOAD_FOLDER, file.filename)
+            safe_name = secure_filename(file.filename)
+
+            # 🔥 make filename unique every time
+            unique_name = f"{int(time.time())}_{safe_name}"
+
+            save_path = os.path.join(UPLOAD_FOLDER, unique_name)
             file.save(save_path)
-            session['filename'] = file.filename
+
+            # store unique filename in session
+            session['filename'] = unique_name
 
     return render_template('home.html')
